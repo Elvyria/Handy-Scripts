c660f990a139c13060c080acdfc6ea1f7de773ed
Elvyria <elvyria@tutanota.com>
2022-01-03 23:56:34 +0300

    Changed help message format, explicit function arguments

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,27 +2,40 @@
 
 # Create Filesystem Backup
 
+usage () {
+	printf '
+Usage:
+    [sudo -E] backup.sh SOURCE DESTINATION
+'
+
+	exit
+}
+
+backup () {
+	# ARGS: --------
+	source="$1"
+	destination="$2"
+	#---------------
+
+	. /etc/os-release
+
+	date=$(date "+%F")
+	backup="$destination/$ID-backup-$date.tar.gz"
+
+	if [ -f "$XDG_CONFIG_HOME/backup.ignore" ]; then
+		exclude="-X$XDG_CONFIG_HOME/backup.ignore"
+	fi
+
+	# Compression method
+	if command -v "pbzip2"; then
+		method='-I pbzip2'
+	fi
+
+	tar "$exclude" --acls --xattrs -cpPvf "$backup" "$method" "$source"
+}
+
 if [ -z "$2" ] || [ ! -d "$1" ] || [ ! -d "$2" ]; then
-	printf "[sudo -E] backup.sh SOURCE DESTINATION"
-
-	exit 1
+	usage
 fi
 
-source="$1"
-destination="$2"
-
-. /etc/os-release
-
-date=$(date "+%F")
-backup="$destination/$ID-backup-$date.tar.gz"
-
-if [ -f "$XDG_CONFIG_HOME/backup.ignore" ]; then
-	exclude="-X$XDG_CONFIG_HOME/backup.ignore"
-fi
-
-# Compression method
-if command -v "pbzip2"; then
-	method='-I pbzip2'
-fi
-
-tar "$exclude" --acls --xattrs -cpPvf "$backup" "$method" "$source"
+backup "$1" "$2"
