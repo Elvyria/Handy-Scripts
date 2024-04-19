e81ccb1bb6fe0b48c4cbb900d1bcc09e6eb10fb7
Elvyria <elvyria@tutanota.com>
2024-04-19 11:48:57 +0300

    whoami.sh: replaced `hostname` -> `ip addr`, changed flag implementation for printing only required info

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 
 ## Scripts
 * [**backup.sh**](scripts/backup.sh) packs files in archive, preserving all file attributes
-* [**firejail-default.sh**](scripts/firejail-default.sh) helps you manage links to use [firejail](https://github.com/netblue30/firejail) by default for certain programs
 * [**morph.sh**](scripts/morph.sh) perform simple file conversions
 * [**playback.zsh**](scripts/playback.zsh) advanced media control
 * [**sound-tricks.sh**](scripts/sound-tricks.sh) has various sound server control related functions
 * [**whoami.sh**](scripts/whoami.sh) prints IP information
 
 ### Shared Dependencies
+* [perl](https://www.cpan.org/src/README.html)
 * [ripgrep](https://github.com/BurntSushi/ripgrep) (rg)
--- scripts/whoami.sh
+++ scripts/whoami.sh
@@ -9,60 +9,93 @@ normal=$(tput sgr0)
 usage () {
 	printf '
 Usage:
-    whoami.sh [opts]
+    whoami.sh [--help] [[-l, -p] [-4] [-6]]
 
 OPTIONS
     -h, --help   print this message
     -l, --local  print local IP
-    -4, --IPv4   print public IPv4
-    -6, --IPv6   print public IPv6
+	-p, --public print public IP
+    -4, --IPv4   output only IPv4
+    -6, --IPv6   output only IPv6
 '
 	exit
 }
 
-_local () {
-	hostname -i
+_local_v4 () {
+	ip -c=never -4 -br addr show | perl -pge 's/.*?UP +(.+?)[\r\n\$]+.*/$1/s;s/\/24//'
 }
 
-ipv4 () {
+_local_v6 () {
+	ip -c=never -6 -br addr show | perl -pge 's/.*?UP +(.+?)[\r\n\$]+.*/$1/s;s/\/64//'
+}
+
+_public_v4 () {
 	curl -m 1 -s -4 "$provider"
 }
 
-ipv6 () {
+_public_v6 () {
 	curl -m 1 -s -6 "$provider"
 }
 
 info () {
-	local=$(_local)
-	IPv4=$(ipv4)
-	IPv6=$(ipv6)
+	localV4=$(_local_v4)
+	localV6=$(_local_v6)
 
-	if [ -n "$local" ]; then
-		printf "%sLocal%s:\n    IPv4: %s\n" "${yellow}" "${normal}" "$local"
+	if [ -n "$localV4" ] || [ -n "$localV6" ]; then \
+		printf "%sLocal%s:\n" "${yellow}" "${normal}"
+
+		[ -n "$localV4" ] && printf '    IPv4: %s\n' "$localV4"
+		[ -n "$localV6" ] && printf '    IPv6: %s\n' "$localV6"
 	fi
 
-	if [ -n "$IPv4" ] || [ -n "$IPv6" ]; then \
+	publicV4=$(_public_v4)
+	publicV6=$(_public_v6)
+
+	if [ -n "$publicV4" ] || [ -n "$publicV6" ]; then \
 		printf "%sPublic%s:\n" "${yellow}" "${normal}"
 
-		[ -n "$IPv4" ] && printf '    IPv4: %s\n' "$IPv4"
-		[ -n "$IPv6" ] && printf '    IPv6: %s\n' "$IPv6"
+		[ -n "$publicV4" ] && printf '    IPv4: %s\n' "$publicV4"
+		[ -n "$publicV6" ] && printf '    IPv6: %s\n' "$publicV6"
 	fi
 }
 
-case "$1" in
-	-h | --help)
-		usage
-		;;
-	-l | --local)
-		_local
-		;;
-	-4 | --[Ii][Pp][Vv]4)
-		ipv4
-		;;
-	-6 | --[Ii][Pp][Vv]6)
-		ipv6
-		;;
-	*)
-		info
-		;;
-esac
+asked_public=''
+asked_local=''
+asked_v4=''
+asked_v6=''
+
+[ $# -eq 0 ] && info && exit
+
+for arg; do
+	case "$arg" in
+		-4 | --[Ii][Pp][Vv]4) asked_v4=true ;;
+		-6 | --[Ii][Pp][Vv]6) asked_v6=true ;;
+		-l | --local) asked_local=true ;;
+		-p | --public) asked_public=true ;;
+		-h | --help) usage ;;
+	esac
+done
+
+if [ -n "$asked_local" ]; then
+	if [ -z "$asked_v4" ] && [ -z "$asked_v6" ]; then
+		_local_v4
+		echo
+		_local_v6
+		exit
+	fi
+
+	[ -n "$asked_v4" ] && _local_v4 && echo
+	[ -n "$asked_v6" ] && _local_v6 && echo
+fi
+
+if [ -n "$asked_public" ]; then
+	if [ -z "$asked_v4" ] && [ -z "$asked_v6" ]; then
+		_public_v4
+		echo
+		_public_v6
+		exit
+	fi
+
+	[ -n "$asked_v4" ] && _public_v4 && echo
+	[ -n "$asked_v6" ] && _public_v6 && echo
+fi
